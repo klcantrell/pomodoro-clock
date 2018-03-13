ade8c5fffdde5667001ca2c8d33f75b0e98c583a
Kalalau <cantrellkalalau@gmail.com>
2018-03-13 14:14:33 -0400

    loading fonts locally - prepping to load from AWS

--- webpack.config.js
+++ webpack.config.js
@@ -44,6 +44,17 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.(ttf|eot|woff|woff2)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+            // publicPath: ''
+          }
+        }
       }
     ]
 	},
