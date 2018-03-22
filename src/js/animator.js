bc93c422894cf5666e0d63a2c4054b03ac17129c
Kalalau <cantrellkalalau@gmail.com>
2018-03-22 16:50:03 -0400

    refactor to use KUTE's manipulation of SVG transform attribute rather than CSS.  also fixed extra animation that was occuring on line and circle SVG elements

--- src/js/animator.js
+++ src/js/animator.js
@@ -11,7 +11,7 @@ const animator = {
   line: '',
   init({initTime, view}) {
     this.deps.view = view;
-    this.deps.view.triangle.style.transform = "rotate(0)";
+    this.deps.view.triangle.setAttribute("transform", "rotate(0 290 4)");
     this.deps.view.circle.style["stroke-dasharray"] = 0;
     this.deps.view.circle.setAttribute("stroke", "rgba(71, 61, 255, 0.47)");
     this.deps.view.line.setAttribute("stroke", "rgba(184, 184, 184, 0.79)");
@@ -21,7 +21,7 @@ const animator = {
       this.triangle.stop();
       this.circle.stop();
       this.line.stop();
-      this.deps.view.triangle.style.transform = "rotate(0)";
+      this.deps.view.triangle.setAttribute("transform", "rotate(0 290 4)");
       this.deps.view.circle.style["stroke-dasharray"] = 0;
       this.deps.view.circle.setAttribute("stroke", "rgba(71, 61, 255, 0.47)");
       this.deps.view.line.setAttribute("stroke", "rgba(184, 184, 184, 0.79)");
@@ -38,9 +38,9 @@ const animator = {
     this.line.stop();
 
     let ms = minutes * 60000;
-    this.triangle = KUTE.fromTo("#triangle", {rotate: 360}, {rotate: 0}, {duration: ms, easing: "easingCubicOut"});
-    this.circle = KUTE.fromTo("#circle", {draw: "0% 0%", attr: {stroke: "rgba(214, 214, 214, 0.76)"}}, {draw: "0% 100%", attr: {stroke: "rgba(71, 61, 255, 0.47)"}}, {duration: ms < 60000 ? ms : 60000, repeat: minutes});
-    this.line = KUTE.fromTo("#line", {attr: {stroke: "rgba(71, 61, 255, 0.47)"}}, {attr: {stroke: "rgba(184, 184, 184, 0.79)"}}, {duration: 1000, repeat: (ms / 1000), yoyo: true});
+    this.triangle = KUTE.fromTo(this.deps.view.triangle, {svgTransform: {rotate: 360}}, {svgTransform: {rotate: 0}}, {duration: ms, easing: "easingCubicOut"});
+    this.circle = KUTE.fromTo(this.deps.view.circle, {draw: "0% 0%", attr: {stroke: "rgba(214, 214, 214, 0.76)"}}, {draw: "0% 100%", attr: {stroke: "rgba(71, 61, 255, 0.47)"}}, {duration: ms < 60000 ? ms : 60000, repeat: minutes - 1});
+    this.line = KUTE.fromTo(this.deps.view.line, {attr: {stroke: "rgba(71, 61, 255, 0.47)"}}, {attr: {stroke: "rgba(184, 184, 184, 0.79)"}}, {duration: 1000, repeat: (ms / 1000) - 3, yoyo: true});
 
     this.triangle.start();
     this.circle.start();
@@ -48,9 +48,9 @@ const animator = {
   },
   setTweens: function(minutes) {
     let ms = minutes * 60000;
-    this.triangle = KUTE.fromTo("#triangle", {rotate: 0}, {rotate: 360}, {duration: ms, easing: "easingCubicOut"});
-    this.circle = KUTE.fromTo("#circle", {draw: "0% 100%", attr: {stroke: "rgba(71, 61, 255, 0.47)"}}, {draw: "0% 0%", attr: {stroke: "rgba(214, 214, 214, 0.76)"}}, {duration: ms < 60000 ? ms : 60000, repeat: minutes});
-    this.line = KUTE.fromTo("#line", {attr: {stroke: "rgba(184, 184, 184, 0.79)"}}, {attr: {stroke: "rgba(71, 61, 255, 0.47)"}}, {duration: 1000, repeat: (ms / 1000), yoyo: true});
+    this.triangle = KUTE.fromTo(this.deps.view.triangle, {svgTransform: {rotate: 0}}, {svgTransform: {rotate: 360}}, {duration: ms, easing: "easingCubicOut"});
+    this.circle = KUTE.fromTo(this.deps.view.circle, {draw: "0% 100%", attr: {stroke: "rgba(71, 61, 255, 0.47)"}}, {draw: "0% 0%", attr: {stroke: "rgba(214, 214, 214, 0.76)"}}, {duration: ms < 60000 ? ms : 60000, repeat: minutes - 1});
+    this.line = KUTE.fromTo(this.deps.view.line, {attr: {stroke: "rgba(184, 184, 184, 0.79)"}}, {attr: {stroke: "rgba(71, 61, 255, 0.47)"}}, {duration: 1000, repeat: (ms / 1000) - 3, yoyo: true});
   }
 };
 
